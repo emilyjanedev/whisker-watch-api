89575576b311fb61534605c1de00c242db753f3c
Emily Schur <ejschur@gmail.com>
2024-11-30 23:27:59 -0800

    Remove comments

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,16 +1,5 @@
 import multer, { memoryStorage } from "multer";
-import path from "path";
 
-// const storage = multer.diskStorage({
-//   destination: (_req, _file, cb) => {
-//     cb(null, "./public/images");
-//   },
-//   filename: (_req, file, cb) => {
-//     const extname = path.extname(file.originalname);
-//     const basename = path.basename(file.originalname, extname);
-//     cb(null, `${basename}${extname}`);
-//   },
-// });
 const upload = multer({ storage: memoryStorage() });
 
 export { upload };
