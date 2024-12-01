9e061b1084ef25eb1610b4a1b9a7eba439eaa2fb
Emily Schur <ejschur@gmail.com>
2024-12-01 12:20:50 -0800

    Update mock data

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,19 +6,32 @@ export async function seed(knex) {
   await knex("users").del();
   await knex("users").insert([
     {
-      id: "jJNQLjADdLblIvGXk8dpJvOtOS22",
+      id: "2aOKwRsZmjXwDBkc9j4cmQUTAy02",
       user_name: "Zak",
       user_email: "zak@gmail.com",
+      created_at: "2024-12-01 19:37:56",
+      updated_at: "2024-12-01 19:37:56",
     },
     {
-      id: "k6oPVvVOV1g4EWgDCwDoDRcdAWF3",
-      user_name: "Emily",
-      user_email: "ejschur@gmail.com",
-    },
-    {
-      id: "9iVCNJjSEPVwqnnsvjdVE8Besqa2",
+      id: "8UhkK5moosPAeVKVgpDcfvRi5zw1",
       user_name: "Jane",
       user_email: "jane@gmail.com",
+      created_at: "2024-12-01 12:04:42",
+      updated_at: "2024-12-01 12:04:42",
+    },
+    {
+      id: "JjxS0CSLoEXDEPIdPYCogPh4X1Y2",
+      user_name: "Emily",
+      user_email: "ejschur@gmail.com",
+      created_at: "2024-12-01 19:11:54",
+      updated_at: "2024-12-01 19:11:54",
+    },
+    {
+      id: "pCMhyEfAtshKzSxlF8bkpfrXzKK2",
+      user_name: "Developer",
+      user_email: "developer@gmail.com",
+      created_at: "2024-12-01 19:22:23",
+      updated_at: "2024-12-01 19:22:23",
     },
   ]);
 }
